beeb88fd9bd48082ae71e26ac4d2eb2be5e79fb0
Allen Fordham <allen.s.fordham@gmail.com>
2016-08-10 12:17:04 -0600

    nested API calls, all 3 of them work, but I still need to parse the data and then do all the DOM manipulation

--- js/main.js
+++ js/main.js
@@ -30,79 +30,88 @@ $('#saveThisLoc').on('change', function (e) {
 
 //This is my query to google, finding out the length of the trip with traffic. The following variables will go into the query:
 const KEY = '&key=AIzaSyDpCDHuCq0nnxnIIqkSlk--DU5sZ6lIBFw';
-var timeNow = Date.now();
-// nowPlus= function(hours) {
-//   (timeNow + (3600000 * Number.hours));
-// };
-// console.log(nowPlus(2));
+//google navigation API request parameters:
+//departure_time (either 'now' or an integer in seconds since 1970)
+//I need to export the selected future times to inHours:
+var inHours = 0;
+var departTime;
+nowPlus = function(inHours) {
+  departTime = ((Date.now()) + (3600000 * (inHours)));
+  return departTime;
+};
+console.log(departTime);
+console.log(Date.now());
+console.log ('nowPlus: ' + nowPlus(inHours));
 // console.log('current time in ms: ' + timeNow);
-var departureTime = '&departure_time=' + timeNow;
+var departureTime = '&departure_time=' + departTime;
 var traffic = '&traffic_model=best_guess';
-var duration = null;
-var durationInTraffic = null;
 //Origin is my house on Allison and destination is Galvanize in this example
-var originPrefix = "origin="
-var origin = '39.799395,-105.085765'
-var destPrefix = '&destination='
+var originPrefix = 'origin=';
+var origin = '39.799395,-105.085765';
+var destPrefix = '&destination=';
 var destination = '39.733594,-104.992604';
-var url = 'http://galvanize-cors-proxy.herokuapp.com/https://maps.googleapis.com/maps/api/directions/json?'+originPrefix+ origin + destPrefix + destination + departureTime + traffic + KEY;
+var mapsUrl = 'http://galvanize-cors-proxy.herokuapp.com/https://maps.googleapis.com/maps/api/directions/json?' + originPrefix + origin + destPrefix + destination + departureTime + traffic + KEY;
 // console.log(url);
+
 $.ajax({
   method: 'GET',
   // jsonp: 'callback',
   // dataType: 'jsonp',
-  url: url
+  url: mapsUrl
 }).done(function (results) {
   duration = results.routes[0].legs[0].duration.text;
-  // console.log('duration: ' + duration);
-  // return duration;
+  console.log('Average trip duration: ' + duration);
+
   durationInTraffic = results.routes[0].legs[0].duration_in_traffic.text;
-  // console.log('Duration in traffic: ' + durationInTraffic);
+  console.log('Duration in current traffic: ' + durationInTraffic);
   // return durationInTraffic;
 
-  //parse the results and make another AJAX call here (itunes and wunderground) I'll need tonest the AJAX call inside this function or I won't have access to the duration and durationInTraffic values.
-});
-// These are null in this position because I log them without waiting for the above ajax call to give them values.
-// console.log('Duration in traffic: '+durationInTraffic);
-// console.log('duration: ' + duration);
+  //parse the results and make another AJAX call here (itunes and wunderground) I'll need to nest the AJAX call inside this function or I won't have access to the duration and durationInTraffic values.
 
-//google navigation API request parameters:
-//departure_time (either 'now' or an integer in seconds since 1970)
-
-//This is the iTunes API Call. Looks like it works!
-const ITUNES_URL = 'https://itunes.apple.com/search';
-//XXX change inputs for these two parameters
-var artist = 'stuffyoushouldknow';
-var milliseconds = 10000000;
-var queryString = '?term=' + artist + '&kind=podcast';
-const ITUNES_QUERY = ITUNES_URL + queryString;
-// console.log(URL);
-$.ajax({
-  method: 'GET',
-  jsonp: 'callback',
-  dataType: 'jsonp',
-  url: ITUNES_QUERY
-}).done(function (results) {
-  // console.log(results);
-
-  for (var i = 0; i < results.length; i++) {
-    // console.log('test' + results.results[i].trackTimeMillis);
-    if (results.results[i].trackTimeMillis > 1000000) {
-      // console.log('working so far!');
+  //This is the iTunes API Call. Looks like it works!
+  const ITUNES_URL = 'https://itunes.apple.com/search';
+  //XXX change inputs for these two parameters
+  var artist = 'stuffyoushouldknow';
+  // var milliseconds = 10000000;
+  var milliseconds = (parseInt(durationInTraffic) * 60000);
+  console.log('Your trip will currently have a duration of ' + milliseconds + ', searching for podcasts that match your commute.');
+  var queryString = '?term=' + artist + '&kind=podcast';
+  const ITUNES_QUERY = ITUNES_URL + queryString;
+  // console.log(URL);
+  $.ajax({
+    method: 'GET',
+    jsonp: 'callback',
+    dataType: 'jsonp',
+    url: ITUNES_QUERY
+  }).done(function (results) {
+    // console.log(results);
+    //trying to sort the results and collect a few with acceptable length
+    for (var i = 0; i < results.length; i++) {
+      // console.log('test' + results.results[i].trackTimeMillis);
+      if (results.results[i].trackTimeMillis > 1000000) {
+        console.log('working so far!');
+      }
+      // console.log(results.results[8].trackTimeMillis);
     }
-    // console.log(results.results[8].trackTimeMillis);
-    //parse the results and make another AJAX call here (itunes and wunderground)
-  }
-});
+  });
+  //Wunderground API call for destination weather:
+  var destWeather;
+  var destTemp;
+  var destWeatherImg;
 
-//Wunderground API call for destination weather:
-const WEATHER_URL =
-console.log();
-$.ajax({
-  method: 'GET',
-  jsonp: 'callback',
-  dataType: 'jsonp',
-  url: WEATHER_URL
-}).done(function (results) {
-  console.log(results);
+  const WEATHER_URL = 'http://api.wunderground.com/api/903be07b671ce816/conditions/q/' + destination + '.json';
+  const WUNDERKEY = '903be07b671ce816';
+  console.log();
+  $.ajax({
+    method: 'GET',
+    jsonp: 'callback',
+    dataType: 'jsonp',
+    url: WEATHER_URL
+  }).done(function (results) {
+    destTemp = results.current_observation.feelslike_f;
+    destWeather = results.current_observation.weather;
+    destWeatherImg = results.current_observation.icon_url;
+    console.log('Your destination is currently ' + destWeather + ' and ' + destTemp + ' degrees. ');
+  });
+
 });
